908769a12190e1e9bf4ad8eb3033da890444d4a7
Tănase Hagi <tanase.hagi@gmail.com>
2018-03-18 16:36:45 +0200

    add some clean-ups

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,7 +4,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class DataService {
 
-  private _toDos = new BehaviorSubject(["clean your room", "take out the trash", "cure cancer", "save the world"]);
+private _toDos = new BehaviorSubject([]); // ["clean your room", "take out the trash", "cure cancer", "save the world"]
   toDos = this._toDos.asObservable();
   
   constructor() { }
